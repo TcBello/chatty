8b8a3988bd51e56efcf153c68ed1c2f48adf735c
TcBello <78838480+TcBello@users.noreply.github.com>
2023-08-10 17:50:23 +0800

    ref: changed socket host

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // export const socket = io("http://localhost:3001");
-export const socket = io("wss://tcbello-chatty-api.vercel.app", {
+export const socket = io("https://tcbello-chatty-api.vercel.app/socket.io", {
   transports: ["websocket"],
 });
