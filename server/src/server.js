ef2399744403098f73366aa23ba1e288540e8324
TcBello <78838480+TcBello@users.noreply.github.com>
2023-08-10 13:06:51 +0800

    ref: changed cors origin

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://tcbello-chatty.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST", "PUT"],
   },
 });
